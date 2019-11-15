e13e3021d64eddd96c4515fe73aa049a8a4c0035
Eric Wieser <wieser.eric@gmail.com>
2019-11-15 18:13:37 +0000

    Fix setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-"""
-C++-style type templating for Python
-"""
 from setuptools import setup, find_packages
 import os
 
 version_path = os.path.join('type_templating', '_version.py')
 exec(open(version_path).read())
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='type_templating',
     version=__version__,  # noqa: F821
     license='MIT',
-    description=__doc__,
-    long_description=__doc__,
+    description="C++-style type templating for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Eric Wieser',
     url='http://type_templating.readthedocs.io',
     packages=find_packages(),
@@ -24,6 +24,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
+
+    project_urls={
+        "Bug Tracker": "https://github.com/eric-wieser/type_templating/issues",
+        "Source Code": "https://github.com/eric-wieser/type_templating",
+    },
 
     python_requires='>=3.5',
 )
